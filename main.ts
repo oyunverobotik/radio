f1d0261a76c5fc4afdb5e58b9ccc5d743eea11b6
oyunverobotik <78992206+oyunverobotik@users.noreply.github.com>
2021-02-26 09:04:18 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+function goAhead (m1: number, m2: number, image: Image) {
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    -200,
+    SuperBit.enMotors.M2,
+    200
+    )
+    nn.showImage(0)
+}
 input.onButtonPressed(Button.A, function () {
     radio.sendString("left")
 })
@@ -52,9 +61,19 @@ radio.onReceivedString(function (receivedString) {
 input.onButtonPressed(Button.B, function () {
     radio.sendString("right")
 })
+let nn: Image = null
 WSJoyStick.JoyStickInit()
 radio.setGroup(2)
+nn = images.arrowImage(ArrowNames.North)
+let ss = images.arrowImage(ArrowNames.South)
+let ww = images.arrowImage(ArrowNames.West)
+let ee = images.arrowImage(ArrowNames.East)
 basic.forever(function () {
+    if (WSJoyStick.Listen_Dir(DIR.U) || false) {
+        goAhead(-125, 20, nn)
+    } else {
+    	
+    }
     if (WSJoyStick.Listen_Key(KEY.F)) {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
