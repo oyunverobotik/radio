b2474a7b5e1052968052c49e6cb0cb428d3020b5
oyunverobotik <78992206+oyunverobotik@users.noreply.github.com>
2021-02-28 07:16:12 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,26 @@
 function goAhead (m1: number, m2: number, image: Image) {
     SuperBit.MotorRunDual(
     SuperBit.enMotors.M1,
-    m1,
+    m1 * v,
     SuperBit.enMotors.M2,
-    m2
+    m2 * v
     )
     image.showImage(0)
 }
 input.onButtonPressed(Button.A, function () {
     radio.sendString("left")
 })
+WSJoyStick.onKey(KEY.E, function () {
+    v = v + 0.2
+})
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("straight")
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "left") {
-        goAhead(-125, 10, ww)
+        goAhead(-100, 10, ww)
     } else if (receivedString == "right") {
-        goAhead(10, 125, ee)
+        goAhead(10, 100, ee)
     } else if (receivedString == "straight") {
         goAhead(-200, 200, nn)
     } else if (receivedString == "down") {
@@ -35,40 +38,38 @@ radio.onReceivedString(function (receivedString) {
 input.onButtonPressed(Button.B, function () {
     radio.sendString("right")
 })
+WSJoyStick.onKey(KEY.C, function () {
+    v = v - 0.2
+})
 let ee: Image = null
 let ww: Image = null
 let ss: Image = null
 let nn: Image = null
+let v = 0
 WSJoyStick.JoyStickInit()
 radio.setGroup(2)
+let dir = ""
+let old_dir = ""
+v = 1
 nn = images.arrowImage(ArrowNames.North)
 ss = images.arrowImage(ArrowNames.South)
 ww = images.arrowImage(ArrowNames.West)
 ee = images.arrowImage(ArrowNames.East)
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.U)) {
-        radio.sendString("straight")
+        dir = "straight"
     } else if (WSJoyStick.Listen_Dir(DIR.L)) {
-        radio.sendString("left")
+        dir = "left"
     } else if (WSJoyStick.Listen_Dir(DIR.R)) {
-        radio.sendString("right")
+        dir = "right"
     } else if (WSJoyStick.Listen_Dir(DIR.D)) {
-        radio.sendString("down")
+        dir = "down"
     }
     if (WSJoyStick.Listen_Key(KEY.F)) {
-        radio.sendString("stop")
-        SuperBit.MotorRunDual(
-        SuperBit.enMotors.M1,
-        0,
-        SuperBit.enMotors.M2,
-        0
-        )
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
-            `)
+        dir = "stop"
     }
+    if (old_dir != dir) {
+        radio.sendString(dir)
+    }
+    old_dir = dir
 })
